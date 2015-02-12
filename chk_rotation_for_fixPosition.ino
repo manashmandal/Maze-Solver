22082908ce118477b32932ed3e6cf6ee4509f51a
Manash Mandal <manashmndl@gmail.com>
2015-02-12 20:33:12 +0600

    Update chk_rotation_for_fixPosition.ino

--- chk_rotation_for_fixPosition.ino
+++ chk_rotation_for_fixPosition.ino
@@ -8,7 +8,8 @@ int dir = 0;
 
 //int sensor[] = {55, 56, 57, 58, 59, 60, 61};
 int sensor[] = {62, 61, 60, 59, 58, 57, 56, 55};
-
+int leftMotor[] = {3, 4};
+int rightMotor[] = {5, 6};
 
 
 char pattern[9];
@@ -20,9 +21,71 @@ void setup()
   for (int i = 0; i <8; i++){
     pinMode(sensor[i], INPUT);
   }
-  
-  Serial.println("Ready for fucking!");
 }
+
+
+
+void setMotors(int leftSpeed, int rightSpeed)
+{
+  if (leftSpeed >= 0 && rightSpeed >= 0)
+  {
+   analogWrite(leftMotor[0], leftSpeed);   /*   Forward  */
+   analogWrite(leftMotor[1], 0);
+   analogWrite(rightMotor[0], rightSpeed);
+   analogWrite(rightMotor[1], 0);
+  }
+  
+  else if (leftSpeed < 0 && rightSpeed < 0)
+  {
+    leftSpeed = -leftSpeed;
+    rightSpeed = -rightSpeed;
+    
+    analogWrite(leftMotor[0], 0);
+    analogWrite(leftMotor[1], leftSpeed);
+    analogWrite(rightMotor[0], 0);
+    analogWrite(rightMotor[1], rightSpeed);    /* Backward */
+  }
+  
+  else if (leftSpeed == 0 && rightSpeed >= 0)
+  {
+    analogWrite(leftMotor[0], 0);                     /*Left */
+    analogWrite(leftMotor[1], 0);
+    analogWrite(rightMotor[0], rightSpeed);
+    analogWrite(rightMotor[1], 0);
+  }
+  
+  else if (leftSpeed >= 0 && rightSpeed == 0)
+  
+  {
+    analogWrite(leftMotor[0], leftSpeed);
+    analogWrite(leftMotor[1], 0);
+    analogWrite(rightMotor[0], 0);  /*right */
+    analogWrite(rightMotor[1], 0);
+  }
+  
+  else if (leftSpeed >= 0 && rightSpeed <= 0)
+  {
+    rightSpeed = - rightSpeed;
+    analogWrite(leftMotor[0], leftSpeed);  /* turn clockwise */
+    analogWrite(leftMotor[1], 0);
+    analogWrite(rightMotor[0], 0);
+    analogWrite(rightMotor[1], rightSpeed);
+  }
+  
+  else if (leftSpeed <= 0 && rightSpeed >= 0)
+  {
+    
+    leftSpeed = - leftSpeed;
+    analogWrite(leftMotor[0], 0);
+    analogWrite(leftMotor[1], leftSpeed);   /* turn anti clockwise */
+    analogWrite(rightMotor[0], rightSpeed);
+    analogWrite(rightMotor[1], 0);
+  } 
+  
+}
+
+
+
 
 void getPattern()
 {
@@ -57,16 +120,38 @@ void loop()
    Speed = Serial.parseInt();
    Delay = Serial.parseInt();
    dir = Serial.parseInt();
-     
-    
   }
   
-  if (times != 0)
-  // For debugging purpose
-  Serial.print("Times: " + String(times) + "\n"
-              + "Speed: " + String(Speed) + "\n"
-              + "Delay: " + String (Delay) + "\n"
-              + "Dir: " + String(dir));
-              
-  times = 0;
+
+  
+  if (times != 0 ){
+    while (times > 0){
+      if (dir == 0){
+        setMotors(0, -Speed);
+        delay(Delay);
+        setMotors(0, 0); 
+      }
+      
+      else {
+       setMotors( -Speed, 0);
+       delay(Delay);
+       setMotors(0,0);
+      }
+     times--; 
+     Serial.print("The pattern: ");
+     getPattern();
+    }
+    
+    times = 0;
+ }
+  
+  
+  
+   /* // For debugging purpose
+    Serial.print("Times: " + String(times) + "\n"
+                + "Speed: " + String(Speed) + "\n"
+                + "Delay: " + String (Delay) + "\n"
+                + "Dir: " + String(dir));
+              */
+  
 }
