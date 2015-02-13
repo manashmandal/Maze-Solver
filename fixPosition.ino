0de1195b70ec7897968297aa8204e5b53cebda5c
Manash Mandal <manashmndl@gmail.com>
2015-02-14 04:57:57 +0600

    Update fixPosition.ino
    Updated fixposition and other bugs fixed

--- fixPosition.ino
+++ fixPosition.ino
@@ -1,26 +1,34 @@
 #define THRESHOLD 700
 
 
-String preferredPos_1 = "00111000"; // Keep this up
-String preferredPos_2 = "00011100"; // Or Keep this up
-String preferredPos_3 = "00111100"; // Or this up
+char preferredPos_1[] = "00111000"; // Keep this up
+char preferredPos_2[] = "00011100"; // Or Keep this up
+char preferredPos_3[] = "00111100"; // Or this up
+char preferredPos_4[] = "00011000";
+
+
+char stopPattern[] = "0000000";
 
 /* All leaning left and fix right or clockwise to fix*/
 
-String pos_00011110 = "00011110"; // Leaning left, fix right
-String pos_00001111 = "00001111"; // More Leaning left, fix right
-String pos_00011111 = "00011111"; // More slight leaning left, fix right
-String pos_00000111 = "00000111"; // Most leaning left, fix right
+char pos_00011110[] = "00011110"; // Leaning left, fix right
+char pos_00001111[] = "00001111"; // More Leaning left, fix right
+char pos_00011111[] = "00011111"; // More slight leaning left, fix right
+char pos_00000111[] = "00000111"; // Most leaning left, fix right
+char pos_00001100[] = "00001100"; // fix right
+char pos_11111111[] = "11111111";
 
 
 /* All leaning right and fix left or anticlockwise to fix */
 
-String pos_01111000 = "01111000"; // Leaning right, fix left
-String pos_11110000 = "11110000"; // More Leaning right, fix left
-String pos_11111000 = "11111000"; // More slight leaning right, fix left
-String pos_11100000 = "11100000"; // Most leaning right, fix left
+char pos_01111000[] = "01111000"; // Leaning right, fix left
+char pos_11110000[] = "11110000"; // More Leaning right, fix left
+char pos_11111000[] = "11111000"; // More slight leaning right, fix left
+char pos_11100000[] = "11100000"; // Most leaning right, fix left
+char pos_00110000[] = "00110000"; // fix right
 
-String pattern = "00000000";
+
+char pattern[] = "00000000";
 
 // Sensor and motor initialization
 int sensor[] = {62, 61, 60, 59, 58, 57, 56, 55};
@@ -102,38 +110,56 @@ void getPattern()
       pattern[i] = '1';
     }
   }
-  pattern[8] = '\0';
+  
   Serial.println(pattern);
 }
 
 
 // Check Pattern or match pattern
-int matchPattern(String one, String two)
+/*int strcmp(String one, String two)
 {
   if (one == two) return 1;
   else return 0;
-}
+}*/
 
 // Position fixer
 void fixPosition()
 {
   getPattern();
-  while (matchPattern(pattern,preferredPos_1) == 0 || matchPattern(pattern,preferredPos_2) == 0 || matchPattern(pattern,preferredPos_3) == 0)
+  
+  //bool x;
+  //getPattern();
+  while (strcmp(pattern,preferredPos_1) != 0 || strcmp(pattern,preferredPos_2) != 0 || strcmp(pattern,preferredPos_3) != 0 || strcmp(pattern, preferredPos_4) != 0)
   {  
-    if (matchPattern(pattern, pos_00011110) == 1 || matchPattern(pattern, pos_00001111) == 1 || matchPattern(pattern, pos_00011111) == 1 || matchPattern(pattern, pos_00000111) == 1){
-       setMotors(150, -150);
+    
+    if (strcmp(pattern, pos_00011110) == 0 || strcmp(pattern, pos_00001111) == 0 || strcmp(pattern, pos_00011111) == 0 || strcmp(pattern, pos_00000111) == 0 || strcmp(pattern, pos_00001100) == 0){
+      getPattern();
+     Serial.println("Go to right"); 
+      
+    }
+    /*if (strcmp(pattern, pos_00011110) == 1 || strcmp(pattern, pos_00001111) == 1 || strcmp(pattern, pos_00011111) == 1 || strcmp(pattern, pos_00000111) == 1 || strcmp(pattern, pos_00001100) == 1){
+       setMotors(50, -50);
        delay(50);
-       setMotors(0,0);
+       Serial.println("First condition");
     } 
-    else if (matchPattern(pattern, pos_01111000) == 1 || matchPattern(pattern, pos_11110000) == 1 || matchPattern(pattern, pos_11111000) == 1 || matchPattern(pattern, pos_11100000) == 1)
+    else if (strcmp(pattern, pos_01111000) == 1 || strcmp(pattern, pos_11110000) == 1 || strcmp(pattern, pos_11111000) == 1 || strcmp(pattern, pos_11100000) == 1 || strcmp(pattern, pos_00110000) == 1)
      {
-       setMotors(-150, 150);
+       setMotors(-100, 100);
        delay(50);
-       setMotors(0,0);  
-       }
-   
-    else break;    
-   }  
+       Serial.println("second condition");
+     } */
+     else if (strcmp(pattern, pos_01111000) == 0|| strcmp(pattern, pos_11110000) == 0|| strcmp(pattern, pos_11111000) == 0 || strcmp(pattern, pos_11100000) == 0 || strcmp(pattern, pos_00110000) == 0){
+       getPattern();
+       Serial.println("Go to left");
+     }
+     
+     else {
+       getPattern();
+      Serial.println("Unknown pattern"); 
+     }
+     
+   }
+   setMotors(0,0);  
 }
   
 void setup()
@@ -143,7 +169,13 @@ void setup()
   for (int i = 0; i <8; i++){
     pinMode(sensor[i], INPUT);
   }
+  for (int i = 0; i < 2; i++){
+    pinMode(leftMotor[i], OUTPUT);
+    pinMode(rightMotor[i], OUTPUT);
+  }
+  
   getPattern();
+  delay(2000);
   
 }
 
@@ -151,5 +183,6 @@ void setup()
 void loop()
 {
   getPattern();
-  fixPosition();
+  if (strcmp(pattern, stopPattern) == 0) Serial.println("Bot stopped");
+  else fixPosition();
 }
